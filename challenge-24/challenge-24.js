7cbc6d298ee04c8c3a508c1d2e4550c9a467faf8
Mariana <mari.zangue@protonmail.com>
2018-10-08 21:03:54 -0300

    Exercício corrigido

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -9,13 +9,14 @@ listeners de eventos, etc);
 - faça refactories para melhorar esse código, mas de forma que o mantenha com a
 mesma funcionalidade.
 */
-(function() {
+(function(win, doc) {
+  'use strict';
 
-  var $visor = document.querySelector('[data-js="visor"]');
-  var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-  var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-  var $buttonCE = document.querySelector('[data-js="button-ce"]');
-  var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+  var $visor = doc.querySelector('[data-js="visor"]');
+  var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+  var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+  var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+  var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
 
   function init() {
     Array.prototype.forEach.call($buttonsNumbers, function(button) {
@@ -24,39 +25,31 @@ mesma funcionalidade.
     Array.prototype.forEach.call($buttonsOperations, function(button) {
       button.addEventListener('click', handleClickOperation, false);
     });
-
     $buttonCE.addEventListener('click', handleClickCE, false);
     $buttonEqual.addEventListener('click', handleClickEqual, false);
   }
 
-  init();
-
-  // Função de clicar no número
   function handleClickNumber() {
     reloadInput(this);
   }
 
-  // FUnção de clicar no operador
   function handleClickOperation() {
     $visor.value = removeLastItemIfItIsAnOperator($visor.value);
     reloadInput(this);
   }
 
-  // Função de limpar
   function handleClickCE() {
     $visor.value = 0;
   }
 
-  // Verifica se o último item é um operador
   function isLastItemAnOperation(number) {
-    var operations = ['+', '-', 'x', '÷'];
-    var lastItem = lastItem(number);
+    var operations = getOperations();
+    var last = lastItem(number);
     return operations.some(function(operator) {
-      return operator === lastItem;
+      return operator === last;
     });
   }
 
-  // Remove o último item se ele for um operador
   function removeLastItemIfItIsAnOperator(number) {
     if(isLastItemAnOperation(number)) {
       return selectAllExceptLastItem(number);
@@ -64,47 +57,55 @@ mesma funcionalidade.
     return number;
   }
 
-  // Faz o calculo
   function handleClickEqual() {
     $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-    var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-    $visor.value = allValues.reduce(function(accumulated, actual) {
-      var firstValue = selectAllExceptLastItem(accumulated);
-      var operator = lastItem(accumulated);
-      var lastValue = removeLastItemIfItIsAnOperator(actual);
-      var lastOperator = isLastItemAnOperation(actual) ? lastItem(actual) : '';
-      return applyOperation(operator, firstValue, lastValue, lastOperator);
-    });
+    var allValues = $visor.value.match(getRegexOperations());
+    $visor.value = allValues.reduce(calculateAllValues);
   }
 
-  // dá um reload no Input
+  function getRegexOperations() {
+    return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
+  }
+
+  function calculateAllValues(accumulated, actual) {
+    var firstValue = selectAllExceptLastItem(accumulated);
+    var operator = lastItem(accumulated);
+    var lastValue = removeLastItemIfItIsAnOperator(actual);
+    var lastOperator = isLastItemAnOperation(actual) ? lastItem(actual) : '';
+    return applyOperation(operator, firstValue, lastValue) + lastOperator;
+  }
+
+   function applyOperation(operator, firstValue, lastValue, lastOperator) {
+    switch(operator) {
+      case '+':
+        return Number(firstValue) + Number(lastValue);
+      case '-':
+        return Number(firstValue) - Number(lastValue);
+      case 'x':
+        return Number(firstValue) * Number(lastValue);
+      case '÷':
+        return Number(firstValue) / Number(lastValue);
+    }
+  }
+
   function reloadInput(btn) {
     $visor.value += btn.value;
   }
 
-  // seleciona todos os elementos do array, menos o último
   function selectAllExceptLastItem(prop) {
     return prop.slice(0, -1);
   }
 
-  // retorna o último item
   function lastItem(prop) {
     return prop.split('').pop();
   }
 
-  // Aplica uma operação
-  function applyOperation(operator, firstValue, lastValue, lastOperator) {
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-    }
+  function getOperations() {
+    return Array.prototype.map.call( $buttonsOperations, function(item) {
+      return item.value;
+    } );
   }
 
+  init();
 
-})();
+})(window, document);
