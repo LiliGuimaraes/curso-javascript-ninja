3143d140fc6b43e83220a078536a6357846c7ddb
Mariana <mari.zangue@protonmail.com>
2018-10-10 21:33:17 -0300

    Exercício corrigido

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -19,13 +19,37 @@ selecionados.
 Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
-// ?
+(function(){
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  'use strict';
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  function DOM(element) {
+    this.element = document.querySelectorAll(element);
+  }
+
+  DOM.prototype.on = function(event, callback) {
+    for (var children of this.element) {
+      children.addEventListener(event, callback);
+    }
+  }
+
+  DOM.prototype.off = function(event, callback) {
+    for (var children of this.element) {
+      children.removeEventListener(event, callback);
+    }
+  }
+
+  DOM.prototype.get = function() {
+    return this.element;
+  }
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', function(e) {
+    e.preventDefault();
+    console.log('clicou');
+  });
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})();
